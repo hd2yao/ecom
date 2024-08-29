003083bd54e71173b0ef3b6c0661ced24e20c03e
hd2yao <1054290355@qq.com>
2024-08-29 16:38:16 +0800

    feat: 在主函数中创建 mysql 连接

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,27 @@ package main
 import (
     "log"
 
+    "github.com/go-sql-driver/mysql"
+
     "github.com/hd2yao/ecom/cmd/api"
+    "github.com/hd2yao/ecom/db"
 )
 
 func main() {
+    cfg := mysql.Config{
+        User:                 "root",
+        Passwd:               "root",
+        Addr:                 "127.0.0.1:3306",
+        DBName:               "ecom",
+        Net:                  "tcp",
+        AllowNativePasswords: true,
+        ParseTime:            true,
+    }
+    db, err := db.NewMySQLStorage(cfg)
+    if err != nil {
+        log.Fatal(err)
+    }
+
     server := api.NewAPIServer(":8080", nil)
     if err := server.Run(); err != nil {
         log.Fatal(err)
