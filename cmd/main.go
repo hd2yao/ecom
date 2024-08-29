cd2003b5313faaa1edcd4f0f4200cd4aa251a8bd
hd2yao <1054290355@qq.com>
2024-08-29 17:07:16 +0800

    feat: 在 main 中检查数据库的连接

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "database/sql"
     "log"
 
     "github.com/go-sql-driver/mysql"
@@ -25,8 +26,20 @@ func main() {
         log.Fatal(err)
     }
 
+    // 检查数据库是否连接成功
+    initStorage(db)
+
     server := api.NewAPIServer(":8080", db)
     if err := server.Run(); err != nil {
         log.Fatal(err)
     }
+}
+
+func initStorage(db *sql.DB) {
+    err := db.Ping()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Println("DB: Successfully connected!")
 }
