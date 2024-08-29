d77a64d20b19c6ab6116ebb2f3f3233d972476ba
hd2yao <1054290355@qq.com>
2024-08-29 15:34:08 +0800

    feat: 实现 user 的路由

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,9 +2,12 @@ package api
 
 import (
     "database/sql"
+    "log"
     "net/http"
 
     "github.com/gorilla/mux"
+
+    "github.com/hd2yao/ecom/service/user"
 )
 
 type APIServer struct {
@@ -24,7 +27,13 @@ func (a *APIServer) Run() error {
     // 在这里可以自定义路由及其处理函数
     // 只要实现了接口 Handler 就可以使用
     router := mux.NewRouter()
+    subRouter := router.PathPrefix("/api/v1").Subrouter()
 
+    // 注册 user 功能的路由以及处理函数
+    userHandler := user.NewHandler()
+    userHandler.RegisterRoutes(subRouter)
+
+    log.Println("Listening on", a.addr)
     // 监听服务器 并为路由提供解决方法
     return http.ListenAndServe(a.addr, router)
 }
