4950bce7424d3c745ee874832f1eb7048f628f0f
hd2yao <1054290355@qq.com>
2024-08-30 15:09:20 +0800

    feat: 先实现 UserStore 接口

--- service/user/store.go
+++ service/user/store.go
@@ -15,13 +15,21 @@ func NewStore(db *sql.DB) *Store {
     return &Store{db: db}
 }
 
-func (s *Store) GetUserByEmail(email string) (*types.Users, error) {
+func (s *Store) CreateUser(user types.User) error {
+    return nil
+}
+
+func (s *Store) GetUserByID(id int) (*types.User, error) {
+    return nil, nil
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
     rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
     if err != nil {
         return nil, err
     }
 
-    u := new(types.Users)
+    u := new(types.User)
     for rows.Next() {
         u, err = scanRowsIntoUser(rows)
         if err != nil {
@@ -36,8 +44,8 @@ func (s *Store) GetUserByEmail(email string) (*types.Users, error) {
     return u, nil
 }
 
-func scanRowsIntoUser(rows *sql.Rows) (*types.Users, error) {
-    user := new(types.Users)
+func scanRowsIntoUser(rows *sql.Rows) (*types.User, error) {
+    user := new(types.User)
 
     err := rows.Scan(
         &user.ID,
