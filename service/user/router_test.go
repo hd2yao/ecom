bcd8c78825c0a04030de3303c9b33034fee036d1
hd2yao <1054290355@qq.com>
2024-08-30 17:40:14 +0800

    feat: 编写测试用例

--- service/user/router_test.go
+++ service/user/router_test.go
@@ -1,13 +1,45 @@
 package user
 
 import (
-    "github.com/hd2yao/ecom/types"
+    "bytes"
+    "encoding/json"
+    "net/http"
+    "net/http/httptest"
     "testing"
+
+    "github.com/gorilla/mux"
+
+    "github.com/hd2yao/ecom/types"
 )
 
 func TestUserServiceHandlers(t *testing.T) {
     userStore := &mockUserStore{}
     handler := NewHandler(userStore)
+
+    t.Run("should fail if the user payload is invalid", func(t *testing.T) {
+        payload := types.RegisterUserPayload{
+            FirstName: "user",
+            LastName:  "123",
+            Email:     "asd",
+            Password:  "asd",
+        }
+        marshalled, _ := json.Marshal(payload)
+
+        req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+        if err != nil {
+            t.Fatal(err)
+        }
+
+        rr := httptest.NewRecorder()
+        router := mux.NewRouter()
+
+        router.HandleFunc("/register", handler.handleRegister)
+        router.ServeHTTP(rr, req)
+
+        if rr.Code != http.StatusBadRequest {
+            t.Errorf("expected status code %d, got %d, err %s", http.StatusBadRequest, rr.Code, rr.Body)
+        }
+    })
 }
 
 // 模拟(mock)实现 UserStore 接口
