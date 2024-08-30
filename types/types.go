8604ed6c20a6bd7283e92f1b43ebcd9ff71f4f16
hd2yao <1054290355@qq.com>
2024-08-30 15:00:25 +0800

    feat: 新建 Users 用户结构体

--- types/types.go
+++ types/types.go
@@ -1,5 +1,16 @@
 package types
 
+import "time"
+
+type Users struct {
+    ID        int       `json:"id"`
+    FirstName string    `json:"first_name"`
+    LastName  string    `json:"last_name"`
+    Email     string    `json:"email"`
+    Password  string    `json:"password"`
+    CreatedAt time.Time `json:"created_at"`
+}
+
 type RegisterUserPayload struct {
     FirstName string `json:"first_name"`
     LastName  string `json:"last_name"`
