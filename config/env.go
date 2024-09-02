ee14285af5c73f2daa1353720ca012469d808b06
hd2yao <1054290355@qq.com>
2024-09-02 16:27:57 +0800

    feat: 新增配置 JWT secret 变量

--- config/env.go
+++ config/env.go
@@ -15,6 +15,7 @@ type Config struct {
     DBPassword             string
     DBAddress              string
     DBName                 string
+    JWTSecret              string
     JWTExpirationInSeconds int64
 }
 
@@ -33,6 +34,7 @@ func initConfig() Config {
         DBPassword:             getEnv("DB_PASSWORD", "mypassword"),
         DBAddress:              fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
         DBName:                 getEnv("DB_NAME", "ecom"),
+        JWTSecret:              getEnv("JWT_SECRET", "not-so-secret-now-is-it?"),
         JWTExpirationInSeconds: getEnvAsInt("JWT_EXP", 3600*24*7),
     }
 }
