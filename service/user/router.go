7f7fd922da69f9ea910d332538cd18c49371ca54
hd2yao <1054290355@qq.com>
2024-09-02 16:08:30 +0800

    feat: 完成当前用户登录的逻辑

--- service/user/router.go
+++ service/user/router.go
@@ -1,71 +1,97 @@
 package user
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/go-playground/validator/v10"
-    "github.com/gorilla/mux"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
 
-    "github.com/hd2yao/ecom/service/auth"
-    "github.com/hd2yao/ecom/types"
-    "github.com/hd2yao/ecom/utils"
+	"github.com/hd2yao/ecom/service/auth"
+	"github.com/hd2yao/ecom/types"
+	"github.com/hd2yao/ecom/utils"
 )
 
 type Handler struct {
-    store types.UserStore
+	store types.UserStore
 }
 
 func NewHandler(store types.UserStore) *Handler {
-    return &Handler{store: store}
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
-    router.HandleFunc("/login", h.handleLogin).Methods("POST")
-    router.HandleFunc("/register", h.handleRegister).Methods("POST")
+	router.HandleFunc("/login", h.handleLogin).Methods("POST")
+	router.HandleFunc("/register", h.handleRegister).Methods("POST")
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.LoginUserPayload
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
 
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+		return
+	}
+
+	// check if the user exists
+	u, err := h.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+		return
+	}
+
+	// check if the password correct
+	if !auth.ComparePasswords(u.Password, []byte(payload.Password)) {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid email or password"))
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, map[string]string{"token": ""})
 }
 
 // 用户注册
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
-    // get JSON payload
-    var payload types.RegisterUserPayload
-    if err := utils.ParseJSON(r, &payload); err != nil {
-        utils.WriteError(w, http.StatusBadRequest, err)
-    }
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
 
-    // validate the payload
-    if err := utils.Validate.Struct(payload); err != nil {
-        errors := err.(validator.ValidationErrors)
-        utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
-        return
-    }
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+		return
+	}
 
-    // check if the user exists
-    _, err := h.store.GetUserByEmail(payload.Email)
-    if err == nil {
-        utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
-        return
-    }
+	// check if the user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
 
-    // if it doesn't, we create the new user
-    hashedPassword, err := auth.HashPassword(payload.Password)
-    if err != nil {
-        utils.WriteError(w, http.StatusInternalServerError, err)
-        return
-    }
+	// if it doesn't, we create the new user
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 
-    err = h.store.CreateUser(types.User{
-        FirstName: payload.FirstName,
-        LastName:  payload.LastName,
-        Email:     payload.Email,
-        Password:  hashedPassword,
-    })
-    if err != nil {
-        utils.WriteError(w, http.StatusInternalServerError, err)
-    }
-    utils.WriteJSON(w, http.StatusCreated, nil)
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
