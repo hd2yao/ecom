3b8c75411445ded5b92e7069915c735149f2dcb9
hd2yao <1054290355@qq.com>
2024-09-02 11:19:32 +0800

    feat: 增加验证器的测试

--- service/user/router_test.go
+++ service/user/router_test.go
@@ -1,45 +1,70 @@
 package user
 
 import (
-    "bytes"
-    "encoding/json"
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 
-    "github.com/hd2yao/ecom/types"
+	"github.com/hd2yao/ecom/types"
 )
 
 func TestUserServiceHandlers(t *testing.T) {
-    userStore := &mockUserStore{}
-    handler := NewHandler(userStore)
+	userStore := &mockUserStore{}
+	handler := NewHandler(userStore)
 
-    t.Run("should fail if the user payload is invalid", func(t *testing.T) {
-        payload := types.RegisterUserPayload{
-            FirstName: "user",
-            LastName:  "123",
-            Email:     "asd",
-            Password:  "asd",
-        }
-        marshalled, _ := json.Marshal(payload)
+	t.Run("should fail if the user payload is invalid", func(t *testing.T) {
+		payload := types.RegisterUserPayload{
+			FirstName: "user",
+			LastName:  "123",
+			Email:     "invalid",
+			Password:  "asd",
+		}
+		marshalled, _ := json.Marshal(payload)
 
-        req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
-        if err != nil {
-            t.Fatal(err)
-        }
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
 
-        rr := httptest.NewRecorder()
-        router := mux.NewRouter()
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
 
-        router.HandleFunc("/register", handler.handleRegister)
-        router.ServeHTTP(rr, req)
+		router.HandleFunc("/register", handler.handleRegister)
+		router.ServeHTTP(rr, req)
 
-        if rr.Code != http.StatusBadRequest {
-            t.Errorf("expected status code %d, got %d, err %s", http.StatusBadRequest, rr.Code, rr.Body)
-        }
-    })
+		if rr.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d, got %d, err %s", http.StatusBadRequest, rr.Code, rr.Body)
+		}
+	})
+
+	t.Run("should correctly register the user", func(t *testing.T) {
+		payload := types.RegisterUserPayload{
+			FirstName: "user",
+			LastName:  "123",
+			Email:     "valid@gmail.com",
+			Password:  "asd",
+		}
+		marshalled, _ := json.Marshal(payload)
+
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.handleRegister)
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusCreated {
+			t.Errorf("expected status code %d, got %d, err %s", http.StatusCreated, rr.Code, rr.Body)
+		}
+	})
 }
 
 // 模拟(mock)实现 UserStore 接口
@@ -47,13 +72,13 @@ func TestUserServiceHandlers(t *testing.T) {
 type mockUserStore struct{}
 
 func (m *mockUserStore) GetUserByEmail(email string) (*types.User, error) {
-    return &types.User{}, nil
+	return &types.User{}, nil
 }
 
 func (m *mockUserStore) GetUserByID(id int) (*types.User, error) {
-    return &types.User{}, nil
+	return &types.User{}, nil
 }
 
 func (m *mockUserStore) CreateUser(user types.User) error {
-    return nil
+	return nil
 }
