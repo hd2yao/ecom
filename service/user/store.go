9ca2377bd1789572ea2ea4e808311b0587d0ccf3
hd2yao <1054290355@qq.com>
2024-09-02 15:56:12 +0800

    feat: 用户相关的数据库操作完成

--- service/user/store.go
+++ service/user/store.go
@@ -16,11 +16,33 @@ func NewStore(db *sql.DB) *Store {
 }
 
 func (s *Store) CreateUser(user types.User) error {
+    _, err := s.db.Exec("INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)",
+        user.FirstName, user.LastName, user.Email, user.Password)
+    if err != nil {
+        return err
+    }
     return nil
 }
 
 func (s *Store) GetUserByID(id int) (*types.User, error) {
-    return nil, nil
+    rows, err := s.db.Query("SELECT * FROM users WHERE id = ?", id)
+    if err != nil {
+        return nil, err
+    }
+
+    u := new(types.User)
+    for rows.Next() {
+        u, err = scanRowsIntoUser(rows)
+        if err != nil {
+            return nil, err
+        }
+    }
+
+    if u.ID == 0 {
+        return nil, fmt.Errorf("user not found")
+    }
+
+    return u, nil
 }
 
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
