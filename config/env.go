efbe13f2449c2bfa7e5ebbf681c29e396c3943c8
hd2yao <1054290355@qq.com>
2024-09-02 16:21:14 +0800

    feat: 配置新增 JWT 过期时间

--- config/env.go
+++ config/env.go
@@ -3,17 +3,19 @@ package config
 import (
     "fmt"
     "os"
+    "strconv"
 
     "github.com/joho/godotenv"
 )
 
 type Config struct {
-    PublicHost string
-    Port       string
-    DBUser     string
-    DBPassword string
-    DBAddress  string
-    DBName     string
+    PublicHost             string
+    Port                   string
+    DBUser                 string
+    DBPassword             string
+    DBAddress              string
+    DBName                 string
+    JWTExpirationInSeconds int64
 }
 
 var Envs = initConfig()
@@ -25,12 +27,13 @@ func initConfig() Config {
     godotenv.Load()
 
     return Config{
-        PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
-        Port:       getEnv("PORT", "8080"),
-        DBUser:     getEnv("DB_USER", "root"),
-        DBPassword: getEnv("DB_PASSWORD", "mypassword"),
-        DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
-        DBName:     getEnv("DB_NAME", "ecom"),
+        PublicHost:             getEnv("PUBLIC_HOST", "http://localhost"),
+        Port:                   getEnv("PORT", "8080"),
+        DBUser:                 getEnv("DB_USER", "root"),
+        DBPassword:             getEnv("DB_PASSWORD", "mypassword"),
+        DBAddress:              fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
+        DBName:                 getEnv("DB_NAME", "ecom"),
+        JWTExpirationInSeconds: getEnvAsInt("JWT_EXP", 3600*24*7),
     }
 }
 
@@ -39,4 +42,15 @@ func getEnv(key, fallBack string) string {
         return value
     }
     return fallBack
+}
+
+func getEnvAsInt(key string, fallback int64) int64 {
+    if value, ok := os.LookupEnv(key); ok {
+        i, err := strconv.ParseInt(value, 10, 64)
+        if err != nil {
+            return fallback
+        }
+        return i
+    }
+    return fallback
 }
