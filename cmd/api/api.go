fa223238505b1ea26265a0158bd3c729723a421b
hd2yao <1054290355@qq.com>
2024-09-03 14:33:09 +0800

    feat: 在 api 处注册 product 路由

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,40 +1,46 @@
 package api
 
 import (
-    "database/sql"
-    "log"
-    "net/http"
+	"database/sql"
+	"github.com/hd2yao/ecom/service/product"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 
-    "github.com/hd2yao/ecom/service/user"
+	"github.com/hd2yao/ecom/service/user"
 )
 
 type APIServer struct {
-    addr string
-    db   *sql.DB
+	addr string
+	db   *sql.DB
 }
 
 func NewAPIServer(addr string, db *sql.DB) *APIServer {
-    return &APIServer{
-        addr: addr,
-        db:   db,
-    }
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
 }
 
 func (a *APIServer) Run() error {
-    // 初始化路由 注册所有路由
-    // 在这里可以自定义路由及其处理函数
-    // 只要实现了接口 Handler 就可以使用
-    router := mux.NewRouter()
-    subRouter := router.PathPrefix("/api/v1").Subrouter()
+	// 初始化路由 注册所有路由
+	// 在这里可以自定义路由及其处理函数
+	// 只要实现了接口 Handler 就可以使用
+	router := mux.NewRouter()
+	subRouter := router.PathPrefix("/api/v1").Subrouter()
 
-    // 注册 user 功能的路由以及处理函数
-    userStore := user.NewStore(a.db)
-    userHandler := user.NewHandler(userStore)
-    userHandler.RegisterRoutes(subRouter)
+	// 注册 user 功能的路由以及处理函数
+	userStore := user.NewStore(a.db)
+	userHandler := user.NewHandler(userStore)
+	userHandler.RegisterRoutes(subRouter)
 
-    log.Println("Listening on", a.addr)
-    // 监听服务器 并为路由提供解决方法
-    return http.ListenAndServe(a.addr, router)
+	// 注册 product 功能的路由以及处理函数
+	productStore := product.NewStore(a.db)
+	productHandler := product.NewHandler(productStore)
+	productHandler.RegisterRoutes(subRouter)
+
+	log.Println("Listening on", a.addr)
+	// 监听服务器 并为路由提供解决方法
+	return http.ListenAndServe(a.addr, router)
 }
