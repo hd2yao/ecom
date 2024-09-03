e96bdc864e2434e8fc299bf53e5dae8e4bdddddd
hd2yao <1054290355@qq.com>
2024-09-03 14:24:42 +0800

    feat: product 业务服务初步完成

---
+++ service/product/router.go
@@ -0,0 +1,31 @@
+package product
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/hd2yao/ecom/utils"
+	"net/http"
+
+	"github.com/hd2yao/ecom/types"
+)
+
+type Handler struct {
+	store types.ProductStore
+}
+
+func NewHandler(store types.ProductStore) *Handler {
+	return &Handler{store: store}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/products", h.handleCreateProduct).Methods(http.MethodPost)
+}
+
+func (h *Handler) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
+	ps, err := h.store.GetProducts()
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, ps)
+}
