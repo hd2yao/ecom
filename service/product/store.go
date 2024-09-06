9119d88aed7ed0e981d18f7e1b213efa12acc7ce
hd2yao <1054290355@qq.com>
2024-09-06 15:11:19 +0800

    feat(product/store): 产品实现接口新方法

--- service/product/store.go
+++ service/product/store.go
@@ -2,6 +2,8 @@ package product
 
 import (
     "database/sql"
+    "fmt"
+    "strings"
 
     "github.com/hd2yao/ecom/types"
 )
@@ -28,6 +30,33 @@ func (s *Store) GetProducts() ([]types.Product, error) {
         }
         products = append(products, *p)
     }
+    return products, nil
+}
+
+func (s *Store) GetProductsByIDs(productIDs []int) ([]types.Product, error) {
+    placeholders := strings.Repeat(",?", len(productIDs)-1)
+    query := fmt.Sprintf("SELECT * FROM products WHERE id IN (?%s)", placeholders)
+
+    // convert productIDs to []interface{}
+    args := make([]interface{}, len(productIDs))
+    for i, v := range productIDs {
+        args[i] = v
+    }
+
+    rows, err := s.db.Query(query, args...)
+    if err != nil {
+        return nil, err
+    }
+
+    products := []types.Product{}
+    for rows.Next() {
+        p, err := scanRowsIntoProduct(rows)
+        if err != nil {
+            return nil, err
+        }
+        products = append(products, *p)
+    }
+
     return products, nil
 }
 
