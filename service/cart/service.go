56d601b3aec000c7fd8b83424abe30e70cfa220e
hd2yao <1054290355@qq.com>
2024-09-06 15:20:00 +0800

    feat(service): 新增获取产品 ID 的函数

--- service/cart/service.go
+++ service/cart/service.go
@@ -1,1 +1,18 @@
 package cart
+
+import (
+    "fmt"
+
+    "github.com/hd2yao/ecom/types"
+)
+
+func getCartItemsIDs(items []types.CartCheckoutItem) ([]int, error) {
+    productIDs := make([]int, len(items))
+    for i, item := range items {
+        if item.Quantity <= 0 {
+            return nil, fmt.Errorf("invalid quantity for the product %d", item.ProductID)
+        }
+        productIDs[i] = item.ProductID
+    }
+    return productIDs, nil
+}
